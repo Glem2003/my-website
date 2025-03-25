719d5667b2f89a921424f02713bc6ebbb6cca841
glem <glem920813@gmail.com>
2025-03-25 23:32:02 +0800

    update 0325 -> add img

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,13 @@
 import ElectronicBusinessPlatformDemo from './Electronic-business-platform-Demo.png';
 import notFound from './404 error.png';
 import TaskManger from './Task Management.png';
+import BlogPlatform from './Blog Platform.png'
+import SpotifyDemo from './Spotify-Demo.png'
 
 export {
     ElectronicBusinessPlatformDemo as EC,
     notFound,
-    TaskManger
+    TaskManger,
+    BlogPlatform,
+    SpotifyDemo
 }
